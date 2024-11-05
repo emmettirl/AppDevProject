fcea5bdac8bc890d9c29ea62d4e22d195c922d24
Emmett <emmett.fitzharris@mycit.ie>
2024-11-05 14:40:11 +0000

    initial commit

---
+++ src/main/java/AppDev/Project/AppDevProject/AppDevProjectApplication.java
@@ -0,0 +1,13 @@
+package AppDev.Project.AppDevProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDevProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AppDev/Project/AppDevProject/AppDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package AppDev.Project.AppDevProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
